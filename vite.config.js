c813bfb2b9b0367b8173d7bbf4e5eeda679baa55
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-04-03 22:14:31 -0300

    add base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +7,6 @@ export default defineConfig({
     rollupOptions: {
       input: '/frontend/src/main.jsx'
     }
-  }
+  },
+  base: '/concecionariaa-6lkl5zbbx-guilherme-andrades-projects/',
 })
